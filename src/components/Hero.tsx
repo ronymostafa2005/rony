520c30db429001e820b7b88dbaa2df565317fde9
Rawan Mostafa <mrere0013@gmail.com>
2026-02-04 19:00:30 +0200

    fix home page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -171,21 +171,6 @@ const Hero = () => {
           </motion.div>
         </div>
       </div>
-
-      {/* Scroll Indicator */}
-      <motion.div
-        animate={{ y: [0, 10, 0] }}
-        transition={{ duration: 1.5, repeat: Infinity }}
-        className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
-      >
-        <div className="w-6 h-10 border-2 border-blue-500 rounded-full flex justify-center">
-          <motion.div
-            animate={{ y: [0, 12, 0] }}
-            transition={{ duration: 1.5, repeat: Infinity }}
-            className="w-1 h-2 bg-blue-500 rounded-full mt-2"
-          />
-        </div>
-      </motion.div>
     </section>
   );
 };
